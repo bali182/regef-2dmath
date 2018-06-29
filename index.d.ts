dc94ebb12a06c9089f96bc81e3cd2f36ccf29f0c
Balazs Edes <balazs.edes@workday.com>
2018-06-29 10:59:31 +0100

    fixed dts

--- index.d.ts
+++ index.d.ts
@@ -1,107 +1,120 @@
 export class Point {
-  x: number
-  y: number
-  translate(point: PointLike): Point
-  lineSegmentTo(point: PointLike): Point
-  distanceFromPoint(point: PointLike): Point
+  x: number;
+  y: number;
+  translate(point: PointLike): Point;
+  lineSegmentTo(point: PointLike): Point;
+  distanceFromPoint(point: PointLike): Point;
+  negated(): Point;
 }
 
 export class Rectangle {
-  x: number
-  y: number
-  width: number
-  height: number
+  x: number;
+  y: number;
+  width: number;
+  height: number;
 
-  location(): Point
-  size(): Dimension
-  translate(point: PointLike): Rectangle
-  containsPoint(point: PointLike): boolean
-  containsRectangle(rectangle: RectangleLike): boolean
-  intersection(rectangle: RectangleLike): Rectangle
-  top(): LineSegment
-  right(): LineSegment
-  bottom(): LineSegment
-  left(): LineSegment
-  center(): Point
-  topLeft(): Point
-  topRight(): Point
-  bottomLeft(): Point
-  bottomRight(): Point
+  location(): Point;
+  size(): Dimension;
+  translate(point: PointLike): Rectangle;
+  containsPoint(point: PointLike): boolean;
+  containsRectangle(rectangle: RectangleLike): boolean;
+  intersection(rectangle: RectangleLike): Rectangle;
+  top(): LineSegment;
+  right(): LineSegment;
+  bottom(): LineSegment;
+  left(): LineSegment;
+  center(): Point;
+  topLeft(): Point;
+  topRight(): Point;
+  bottomLeft(): Point;
+  bottomRight(): Point;
 }
 
 export class Dimension {
-  width: number
-  height: number
+  width: number;
+  height: number;
 }
 
 export class LineSegment {
-  x1: number
-  y1: number
-  x2: number
-  y2: number
-  point1(): Point
-  point2(): Point
-  length(): number
-  isHorizontal(): boolean
-  isVertical(): boolean
-  containsPoint(point: Point): boolean
-  intersection(line: LineSegmentLike): Point
-  negated(): Point
+  x1: number;
+  y1: number;
+  x2: number;
+  y2: number;
+  point1(): Point;
+  point2(): Point;
+  length(): number;
+  isHorizontal(): boolean;
+  isVertical(): boolean;
+  containsPoint(point: Point): boolean;
+  intersection(line: LineSegmentLike): Point;
 }
 
 export class Line {
-  x1: number
-  y1: number
-  x2: number
-  y2: number
-  intersection(line: LineLike): Point
+  x1: number;
+  y1: number;
+  x2: number;
+  y2: number;
+  intersection(line: LineLike): Point;
 }
 
 interface PointLike {
-  x: number
-  y: number
+  x: number;
+  y: number;
 }
 
-export function point(x: number, y: number): Point
-export function point(other: PointLike): Point
+export function point(x: number, y: number): Point;
+export function point(other: PointLike): Point;
 
 interface DimensionLike {
-  width: number
-  height: number
+  width: number;
+  height: number;
 }
 
-export function dimension(width: number, height: number): Dimension
-export function dimension(other: DimensionLike): Dimension
+export function dimension(width: number, height: number): Dimension;
+export function dimension(other: DimensionLike): Dimension;
 
 interface RectangleLike {
-  x: number
-  y: number
-  width: number
-  height: number
+  x: number;
+  y: number;
+  width: number;
+  height: number;
 }
 
-export function rectangle(x: number, y: number, width: number, height: number): Rectangle
-export function rectangle(topLeft: PointLike, dimension: DimensionLike): Rectangle
-export function rectangle(other: RectangleLike): Rectangle
+export function rectangle(
+  x: number,
+  y: number,
+  width: number,
+  height: number
+): Rectangle;
+export function rectangle(
+  topLeft: PointLike,
+  dimension: DimensionLike
+): Rectangle;
+export function rectangle(other: RectangleLike): Rectangle;
 
 interface LineSegmentLike {
-  x1: number
-  y1: number
-  x2: number
-  y2: number
+  x1: number;
+  y1: number;
+  x2: number;
+  y2: number;
 }
 
-export function lineSegment(x1: number, y1: number, x2: number, y2: number): LineSegment
-export function lineSegment(p1: PointLike, p2: PointLike): LineSegment
-export function lineSegment(other: LineSegmentLike): LineSegment
+export function lineSegment(
+  x1: number,
+  y1: number,
+  x2: number,
+  y2: number
+): LineSegment;
+export function lineSegment(p1: PointLike, p2: PointLike): LineSegment;
+export function lineSegment(other: LineSegmentLike): LineSegment;
 
 interface LineLike {
-  x1: number
-  y1: number
-  x2: number
-  y2: number
+  x1: number;
+  y1: number;
+  x2: number;
+  y2: number;
 }
 
-export function line(x1: number, y1: number, x2: number, y2: number): Line
-export function line(p1: PointLike, p2: PointLike): Line
-export function line(other: LineLike): Line
+export function line(x1: number, y1: number, x2: number, y2: number): Line;
+export function line(p1: PointLike, p2: PointLike): Line;
+export function line(other: LineLike): Line;
